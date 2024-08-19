d21b662c6f89afe53161fad46e4888020d782cac
Ivan Lamonega <ivanlamonega001@gmail.com>
2024-08-19 10:28:10 -0300

    api

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require('dotenv').config(); // Cargar las variables del archivo .env
 
-mongoose.connect(`mongodb://127.0.0.1:27017/Node`)
-.then(()=>{console.log("Conectado")})
-.catch((error=>console.log(error)))
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("Conectado");
+})
+.catch((error) => console.log(error));
 
-module.exports = mongoose
+module.exports = mongoose;
